c7bef11b9827a582a76ca71ddbce07ca80c2e9cf
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-22 08:58:41 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            byte count = 0;
+
+            for (int i = 0; i < 260; i++)
+            {
+                count++;
+            }
+
+            Console.WriteLine(count);  // Overflow of the Type /
+
+        }
+    }
+}
